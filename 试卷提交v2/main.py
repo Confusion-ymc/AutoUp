ebeb871528d0d837626a739917f73173ee447313
Confusion-YMC <1107238486@qq.com>
2025-07-02 20:06:09 +0800

    尝试点击 我知道了

--- 试卷提交v2/main.py
+++ 试卷提交v2/main.py
@@ -177,8 +177,9 @@ class AutoBrowserUpload:
             except:
                 continue
             if '操作成功' in tips_message and '上传成功' in tips_message:
-                if "已经有用户上传了，不能重复上传呢!" in tips_message:
+                if "已经有用户上传了，不能重复上传呢!" in tips_message or "可前往【我的上传】或【个人中心-我的上传】看审核进度！" in tips_message:
                     self.page.locator('.msgtip:visible').get_by_text('我知道了', exact=True).click()
+                if "已经有用户上传了，不能重复上传呢!" in tips_message:
                     raise MyRepeatError(f'部分重复! {tips_message}')
                 task_manager.logger.log(f'[{self.thread_name}] [上传成功] {file_path.name}')
                 return True
